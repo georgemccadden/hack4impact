2725c10e93817d940aa4f97d55e9afa791983d1c
@georgemccadden <georgemccadden@gmail.com>
2022-02-10 23:43:18 -0800

    Making sure that all of the components are in ES6 syntax

--- blackmothersfilm/src/components/Footer.js
+++ blackmothersfilm/src/components/Footer.js
@@ -1,12 +1,17 @@
 import React from "react";
 import backToTheTop from "../assets/back-to-top-arrow.png";
 
-export default function Footer() {
-  return (
-    <div>
-      <a href="#header" className="back-to-the-top">
-        <img src={backToTheTop} />
-      </a>
-    </div>
-  );
+export default class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <a href="#header" className="back-to-the-top">
+          <img src={backToTheTop} />
+        </a>
+      </div>
+    );
+  }
 }
--- blackmothersfilm/src/components/Main.js
+++ blackmothersfilm/src/components/Main.js
@@ -2,21 +2,26 @@ import React from "react";
 import tallRectangle from "../assets/rectangle.png";
 import wideRectangle from "../assets/wide-rectangle.png";
 
-export default function Main() {
-  return (
-    <div>
-      <h5>Sept. 7th, 2017</h5>
-      <h1>Article Heading</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pharetra
-        sapien quis lacus euismod mollis. Praesent lorem erat, gravida quis
-        tellus eu, bibendum pharetra sapien. Morbi lobortis leo quis ultricies
-        hendrerit. Vivamus hendrerit, ante ut efficitur maximus, leo magna
-        venenatis augue
-      </p>
-      <img src={tallRectangle} />
-      <img src={wideRectangle} />
-      <img src={wideRectangle} />
-    </div>
-  );
+export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h5>Sept. 7th, 2017</h5>
+        <h1>Article Heading</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          pharetra sapien quis lacus euismod mollis. Praesent lorem erat,
+          gravida quis tellus eu, bibendum pharetra sapien. Morbi lobortis leo
+          quis ultricies hendrerit. Vivamus hendrerit, ante ut efficitur
+          maximus, leo magna venenatis augue
+        </p>
+        <img src={tallRectangle} />
+        <img src={wideRectangle} />
+        <img src={wideRectangle} />
+      </div>
+    );
+  }
 }
